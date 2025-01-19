91da436b25e581db0359a9e1aadef57a6d5cd077
Josh Stillman <joshstillman@gmail.com>
2025-01-19 17:19:44 -0500

    add text for 16

--- src/utils/wilhelm.tsx
+++ src/utils/wilhelm.tsx
@@ -2787,87 +2787,241 @@ The difficulties at the beginning are too great for some persons. They get stuck
   },
   16: {
     title: '16. Yü / Enthusiasm ䷏',
-
-    text: `above CHêN THE AROUSING, THUNDER
-below K'UN THE RECEPTIVE, EARTH
-
-The strong line in the fourth place, that of the leading official, meets with response and obedience from all the other lines, which are all weak. The attribute of the upper trigram, Chên, is movement; the attributes of K'un, the lower, are obedience and devotion. This begins a movement that meets with devotion and therefore inspires enthusiasm, carrying all with it. Of great importance, furthermore, is the law of movement along the line of least resistance, which in this hexagram is enunciated as the law for natural events and for human life.
-
-THE JUDGMENT
-
-ENTHUSIASM. It furthers one to install helpers
-And to set armies marching.
-
-The time of ENTHUSIASM derives from the fact that there is at hand an eminent man who is in sympathy with the spirit of the people and acts in accord with it. Hence he finds universal and willing obedience. To arouse enthusiasm it is necessary for a man to adjust himself and his ordinances to the character of those whom he has to lead. The inviolability of natural laws rests on this principle of movement along the line of least resistance. Theses laws are not forces external to things but represent the harmony of movement immanent in them. That is why the celestial bodies do not deviate from their orbits and why all events in nature occur with fixed regularity. It is the same with human society: only such laws are rooted in popular sentiment can be enforced, while laws violating this sentiment merely arouse resentment. Again, it is enthusiasm that enables us to install helpers for the completion of an undertaking without fear of secret opposition. It is enthusiasm too that can unify mass movements, as in war, so that they achieve victory.
-
-THE IMAGE
-
-Thunder comes resounding out of the earth:
-The image of ENTHUSIASM.
-Thus the ancient kings made music
-In order to honor merit,
-And offered it with splendor
-To the Supreme Deity,
-Inviting their ancestors to be present.
-
-When, at the beginning of summer, thunder—electrical energy—comes rushing forth from the earth again, and the first thunderstorm refreshes nature, a prolonged state of tension is resolved. Joy and relief make themselves felt. So too, music has power to ease tension within the heart and to loosen the grip of obscure emotions. The enthusiasm of the heart expresses itself involuntarily in a burst of song, in dance and rhythmic movement of the body. From immemorial times the inspiring effect of the invisible sound that moves all hearts, and draws them together, has mystified mankind. Rulers have made use of this natural taste for music; they elevated and regulated it. Music was looked upon as something serious and holy, designed to purify the feelings of men. It fell to music to glorify the virtues of heroes and thus to construct a bridge to the world of the unseen. In the temple men drew near to God with music and pantomimes (out of this later the theater developed). Religious feeling for the Creator of the world was united with the most sacred of human feelings, that of reverence for the ancestors. The ancestors were invited to these divine services as guests of the Ruler of Heaven and as representatives of humanity in the higher regions. This uniting of the human past with the Divinity in solemn moments of religious inspiration established the bond between God and man. The ruler who revered the Divinity in revering his ancestors became thereby the Son of Heaven, in whom the heavenly and the earthly world met in mystical contact. These ideas are the final summation of Chinese culture. Confucius has said of the great sacrifice at which these rites were performed: "He who could wholly comprehend this sacrifice could rule the world as though it were spinning on his hand."
-`,
+    text: (
+      <>
+        The strong line in the fourth place, that of the leading official, meets
+        with response and obedience from all the other lines, which are all
+        weak. The attribute of the upper trigram, Chên, is movement; the
+        attributes of K’un, the lower, are obedience and devotion. This begins a
+        movement that meets with devotion and therefore inspires enthusiasm,
+        carrying all with it. Of great importance, furthermore, is the law of
+        movement along the line of least resistance, which in this hexagram is
+        enunciated as the law for natural events and for human life.
+        <br />
+        <br />
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE JUDGMENT
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;ENTHUSIASM. It furthers one to install helpers
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And to set armies marching.
+          <br />
+          <br />
+        </p>
+        The time of ENTHUSIASM derives from the fact that there is at hand an
+        eminent man who is in sympathy with the spirit of the people and acts in
+        accord with it. Hence he finds universal and willing obedience. To
+        arouse enthusiasm it is necessary for a man to adjust himself and his
+        ordinances to the character of those whom he has to lead. The
+        inviolability of natural laws rests on this principle of movement along
+        the line of least resistance. These laws are not forces external to
+        things but represent the harmony of movement immanent in them. That is
+        why the celestial bodies do not deviate from their orbits and why all
+        events in nature occur with fixed regularity. It is the same with human
+        society: only such laws as are rooted in popular sentiment can be
+        enforced, while laws violating this sentiment merely arouse resentment.
+        <br />
+        <br />
+        Again, it is enthusiasm that enables us to install helpers for the
+        completion of an undertaking without fear of secret opposition. It is
+        enthusiasm too that can unify mass movements, as in war, so that they
+        achieve victory.
+        <br />
+        <br />
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE IMAGE
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Thunder comes resounding out of the earth:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The image of ENTHUSIASM.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Thus the ancient kings made music
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;In order to honor merit,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And offered it with splendor
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;To the Supreme Deity,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Inviting their ancestors to be present.
+          <br />
+          <br />
+        </p>
+        When, at the beginning of summer, thunder—electrical energy—comes
+        rushing forth from the earth again, and the first thunderstorm refreshes
+        nature, a prolonged state of tension is resolved. Joy and relief make
+        themselves felt. So too, music has power to ease tension within the
+        heart and to loosen the grip of obscure emotions. The enthusiasm of the
+        heart expresses itself involuntarily in a burst of song, in dance and
+        rhythmic movement of the body. From immemorial times the inspiring
+        effect of the invisible sound that moves all hearts, and draws them
+        together, has mystified mankind.
+        <br />
+        <br />
+        Rulers have made use of this natural taste for music; they elevated and
+        regulated it. Music was looked upon as something serious and holy,
+        designed to purify the feelings of men. It fell to music to glorify the
+        virtues of heroes and thus to construct a bridge to the world of the
+        unseen. In the temple men drew near to God with music and pantomimes
+        (out of this later the theater developed). Religious feeling for the
+        Creator of the world was united with the most sacred of human feelings,
+        that of reverence for the ancestors. The ancestors were invited to these
+        divine services as guests of the Ruler of Heaven and as representatives
+        of humanity in the higher regions. This uniting of the human past with
+        the Divinity in solemn moments of religious inspiration established the
+        bond between God and man. The ruler who revered the Divinity in revering
+        his ancestors became thereby the Son of Heaven, in whom the heavenly and
+        the earthly world met in mystical contact.
+        <br />
+        <br />
+        These ideas are the final summation of Chinese culture. K'ung Fu-tzu has
+        said of the great sacrifice at which these rites were performed: “He who
+        could wholly comprehend this sacrifice could rule the world as though it
+        were spinning on his hand.”
+        <br />
+        <br />
+      </>
+    ),
     lines: [
-      `Six at the beginning means:
-
-Enthusiasm that expresses itself
-Brings misfortune.
-
-A man in an inferior position has aristocratic connections about which he boasts enthusiastically. This arrogance inevitably invites misfortune.
-Enthusiasm should never be an egotistic emotion; it is justified only when it is a general feeling that unites one with others.
-`,
-      `Six in the second place means:
-
-Firm as a rock. Not a whole day.
-Perseverance brings good fortune.
-
-This describes a person who does not allow himself to be misled by any illusions. While others are letting themselves be dazzled by enthusiasm, he recognizes with perfect clarity the first signs of the time. Thus he neither flatters those above nor neglects those beneath him; he is as firm as a rock. When the first sign of discord appears, he knows the right moment for withdrawing and does not delay even for a day. Perseverance in such conduct will bring good fortune. Confucius says about this line:
-
-To know the seeds, that is divine indeed. In his association with those above him, the superior man does not flatter. In his association with those beneath him, he is not arrogant. For he knows the seeds. The seeds are the first imperceptible beginning of movement, the first trace of good fortune (or misfortune) that shows itself. The superior man perceives the seeds and immediately takes actin. He does not wait even a whole day. In the Book of Changes it is said: "Firm as a rock. Not a whole day. Perseverance brings good fortune."
-
-Firm as a rock, what need of a whole day?
-
-. The judgment can be known.
-
-. The superior man knows what is hidden and what is evident.
-He knows weakness, he knows strength as well.
-Hence the myriads look up to him.
-`,
-      `Six in the third place means:
-
-Enthusiasm that looks upward creates remorse.
-Hesitation brings remorse.
-
-This line is the opposite of the preceding one: the latter bespeaks self-reliance, while here there is enthusiastic looking up to a leader. If a man hesitates too long, this also will bring remorse. The right moment for approach must be seized: only then will he do the right thing.
-`,
-      `Nine in the fourth place means:
-
-The source of enthusiasm.
-He achieves great things.
-Doubt not.
-You gather friends around you
-As a hair clasp gathers the hair.
-
-This describes a man who is able to awaken enthusiasm through his own sureness and freedom from hesitation. He attracts people because he has no doubts and is wholly sincere. Owing to his confidence in them he wins their enthusiastic co-operation and attains success. Just as a clasp draws the hair together and hold it, so he draws man together by the support he gives them.
-`,
-      `Six in the fifth place means:
-
-Persistently ill, and still does not die.
-
-Here enthusiasm is obstructed. A man is under constant pressure, which prevents him from breathing freely. However, this pressure has its advantage—it prevents him from consuming his powers in empty enthusiasm. Thus constant pressure can actually serve to keep one alive.
-`,
-      `Six at the top means:
-Deluded enthusiasm.
-But if after completion one changes,
-There is no blame.
-
-It is a bad thing for a man to let himself be deluded by enthusiasm. But if this delusion has run its course, and he is still capable of changing, then he is freed of error. A sober awakening from false enthusiasm is quite possible and very favorable.
-`,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six at the beginning means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Enthusiasm that expresses itself
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Brings misfortune.
+          <br />
+          <br />
+        </p>
+        A man in an inferior position has aristocratic connections about which
+        he boasts enthusiastically. This arrogance inevitably invites
+        misfortune. Enthusiasm should never be an egotistic emotion; it is
+        justified only when it is a general feeling that unites one with others.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the second place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Firm as a rock. Not a whole day.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Perseverance brings good fortune.
+          <br />
+          <br />
+        </p>
+        This describes a person who does not allow himself to be misled by any
+        illusions. While others are letting themselves be dazzled by enthusiasm,
+        he recognizes with perfect clarity the first signs of the time. Thus he
+        neither flatters those above nor neglects those beneath him; he is as
+        firm as a rock. When the first sign of discord appears, he knows the
+        right moment for withdrawing and does not delay even for a day.
+        Perseverance in such conduct will bring good fortune. K'ung Fu-tzu says
+        about this line:
+        <br />
+        <br />
+        “To know the seeds, that is divine indeed. In his association with those
+        above him, the superior man does not flatter. In his association with
+        those beneath him, he is not arrogant. For he knows the seeds. The seeds
+        are the first imperceptible beginning of movement, the first trace of
+        good fortune (or misfortune) that shows itself. The superior man
+        perceives the seeds and immediately takes action. He does not wait even
+        a whole day. In the Book of Changes it is said: “Firm as a rock. Not a
+        whole day. Perseverance brings good fortune.”
+        <br />
+        <br />
+        Firm as a rock, what need of a whole day?
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE JUDGMENT can be known.
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The superior man knows what is hidden and what
+          is evident.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He knows weakness, he knows strength as well.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Hence the myriads look up to him.”
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the third place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Enthusiasm that looks upward creates remorse.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Hesitation brings remorse.
+          <br />
+          <br />
+        </p>
+        This line is the opposite of the preceding one: the latter bespeaks
+        self-reliance, while here there is enthusiastic looking up to a leader.
+        If a man hesitates too long, this also will bring remorse. The right
+        moment for approach must be seized: only then will he do the right
+        thing.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fourth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The source of enthusiasm.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He achieves great things.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Doubt not.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;You gather friends around you
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;As a hair clasp gathers the hair.
+          <br />
+          <br />
+        </p>
+        This describes a man who is able to awaken enthusiasm through his own
+        sureness and freedom from hesitation. He attracts people because he has
+        no doubts and is wholly sincere. Owing to his confidence in them he wins
+        their enthusiastic cooperation and attains success. Just as a clasp
+        draws the hair together and holds it, so he draws men together by the
+        support he gives them.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the fifth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Persistently ill, and still does not die.
+          <br />
+          <br />
+        </p>
+        Here enthusiasm is obstructed. A man is under constant pressure, which
+        prevents him from breathing freely. However, this pressure has its
+        advantage—it prevents him from consuming his powers in empty enthusiasm.
+        Thus constant pressure can actually serve to keep one alive.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six at the top means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Deluded enthusiasm.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;But if after completion one changes,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;There is no blame.
+          <br />
+          <br />
+        </p>
+        It is a bad thing for a man to let himself be deluded by enthusiasm. But
+        if this delusion has run its course, and he is still capable of
+        changing, he is freed of error. A sober awakening from false enthusiasm
+        is quite possible and very favorable.
+        <br />
+      </>,
     ],
   },
   17: {
