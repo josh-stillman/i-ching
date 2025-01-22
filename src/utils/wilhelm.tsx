d08e1c02357d61a3fbf810b3249f9dbe98573956
Josh Stillman <joshstillman@gmail.com>
2025-01-22 16:25:57 -0500

    add text for 49

--- src/utils/wilhelm.tsx
+++ src/utils/wilhelm.tsx
@@ -9619,87 +9619,224 @@ The difficulties at the beginning are too great for some persons. They get stuck
     ],
   },
   49: {
-    title: '49. Ko / Revolution (Molting)',
+    title: '49. Ko / Revolution (Molting) ䷰',
 
-    text: `above TUI THE JOYOUS, LAKE
-below LI THE CLINGING, FIRE
-
-The Chinese character for this hexagram means in its original sense an animal's pelt, which is changed in the course of the year by molting. From this word is carried over to apply to the "moltings" in political life, the great revolutions connected with changes of governments. The two trigrams making up the hexagram are the same two that appear in K'uei, OPPOSITION 38, that is, the two younger daughters, Li and Tui. But while there the elder of the two daughters is above, and what results is essentially only an opposition of tendencies, here the younger daughter is above. The influences are in actual conflict, and the forces combat each other like fire and water (lake), each trying to destroy the other. Hence the idea of revolution.
-
-THE JUDGMENT
-
-REVOLUTION. On your own day
-You are believed.
-
-Supreme success,
-Furthering through perseverance.
-Remorse disappears.
-
-Political revolutions are extremely grave matters. They should be undertaken only under stress of direst necessity, when there is no other way out. Not everyone is called to this task, but only the man who has the confidence of the people, and even he only when the time is ripe. He must then proceed in the right way, so that he gladdens the people and, by enlightening them, prevents excesses. Furthermore, he must be quite free of selfish aims and must really relieve the need of the people. Only then does he have nothing to regret. Times change, and with them their demands. Thus the seasons change in the course of the year. In the world cycle also there are spring and autumn in the life of peoples and nations, and these call for social transformations.
-
-THE IMAGE
-
-Fire in the lake: the image of REVOLUTION.
-
-Thus the superior man
-
-Sets the calendar in order
-And makes the seasons clear.
-
-Fire below and the lake above combat and destroy each other. So too in the course of the year a combat takes place between the forces of light and the forces of darkness, eventuating in the revolution of the seasons, and man is able to adjust himself in advance to the demands of the different times.
-`,
+    text: (
+      <>
+        The Chinese character for this hexagram means in its original sense an
+        animal’s pelt, which is changed in the course of the year by molting.
+        From this the word is carried over to apply to the “moltings” in
+        political life, the great revolutions connected with changes of
+        governments.
+        <br />
+        <br />
+        The two trigrams making up the hexagram are the same two that appear in
+        K’uei, OPPOSITION (38), that is, the two younger daughters, Li and Tui.
+        But while there the elder of the two daughters is above, and what
+        results is essentially only an opposition of tendencies, here the
+        younger daughter is above. The influences are in actual conflict, and
+        the forces combat each other like fire and water (lake), each trying to
+        destroy the other. Hence the idea of revolution.
+        <br />
+        <br />
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE JUDGMENT
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;REVOLUTION. On your own day
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;You are believed.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Supreme success,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Furthering through perseverance.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Remorse disappears.
+          <br />
+          <br />
+        </p>
+        Political revolutions are extremely grave matters. They should be
+        undertaken only under stress of direst necessity, when there is no other
+        way out. Not everyone is called to this task, but only the man who has
+        the confidence of the people, and even he only when the time is ripe. He
+        must then proceed in the right way, so that he gladdens the people and,
+        by enlightening them, prevents excesses. Furthermore, he must be quite
+        free of selfish aims and must really relieve the need of the people.
+        Only then does he have nothing to regret.
+        <br />
+        <br />
+        Times change, and with them their demands. Thus the seasons change in
+        the course of the year. In the world cycle also there are spring and
+        autumn in the life of peoples and nations, and these call for social
+        transformations.
+        <br />
+        <p>
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;THE IMAGE
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Fire in the lake: the image of
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;REVOLUTION.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Thus the superior man
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Sets the calendar in order
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And makes the seasons clear.
+          <br />
+          <br />
+        </p>
+        Fire below and the lake above combat and destroy each other. So too in
+        the course of the year a combat takes place between the forces of light
+        and the forces of darkness, eventuating in the revolution of the
+        seasons. Man masters these changes in nature by noting their regularity
+        and marking off the passage of time accordingly. In this way order and
+        clarity appear in the apparently chaotic changes of the seasons, and man
+        is able to adjust himself in advance to the demands of the different
+        times.
+        <br />
+        <br />
+      </>
+    ),
     lines: [
-      `Nine at the beginning means:
-
-Wrapped in the hide of a yellow cow.
-
-Changes ought to be undertaken only when there is nothing else to be done. Therefore at first the utmost restraint is necessary. One must becomes firm in one's mind, control oneself-yellow is the color of the means, and the cow is the symbol of docility-and refrain from doing anything for the time being, because any premature offensive will bring evil results.
-`,
-      `Six in the second place means:
-
-When one's own day comes, one may create revolution.
-
-Starting brings good fortune. No blame.
-
-When we have tried in every other way to bring about reforms, but without success, revolution becomes necessary. But such a thoroughgoing upheaval must be carefully prepared. There must be available a man who has the requisite abilities and who possesses public confidence. To such a man we may well turn. This brings good fortune and is not a mistake. The first thing to be considered is our inner attitude toward the new condition that will inevitably come. We have to go out to meet it, as it were. Only in this way can it be prepared for.
-`,
-      `Nine in the third place means:
-
-Starting brings misfortune.
-Perseverance brings danger.
-When talk of revolution has gone the rounds three times,
-One may commit himself,
-And men will believe him.
-
-When change is necessary, there are two mistakes to be avoided. One lies in excessive haste and ruthlessness, which bring disaster. The other lies in excessive hesitation and conservatism, which are also dangerous. Not every demand for change in the existing order should be heeded. On the other hand, repeated and well-founded complaints should not fail of a hearing. When talk of change has come to one's ears three times, and has been pondered well, he may believe and acquiesce in it. Then he will meet with belief and will accomplish something.
-`,
-      `Nine in the fourth place means:
-
-Remorse disappears. Men believe him.
-Changing the form of government brings good fortune.
-
-Radical changes require adequate authority. A man must have inner strength as well as influential position. What he does must correspond with a higher truth and must not spring from arbitrary or petty motives; then it brings great good fortune. If a revolution is not founded on such inner truth, the results are bad, and it has no success. For in the end men will support only those undertakings which they feel instinctively to be just.
-`,
-      `Nine in the fifth place means:
-
-The great man changes like a tiger.
-Even before he questions the oracle
-He is believed.
-
-A tigerskin, with its highly visible black stripes on a yellow ground, shows its distinct pattern from afar. It is the same with a revolution brought about by a great man: large, clear guiding lines become visible, understandable to everyone. Therefore he need not first consult the oracle, for he wins the spontaneous support of the people.
-`,
-      `Six at the top means:
-
-The superior man changes like a panther.
-
-The inferior man molts in the face.
-
-Starting brings misfortune.
-
-To remain persevering brings good fortune.
-
-After the large and fundamental problems are settled, certain minor reforms, and elaborations of these, are necessary. These detailed reforms may be likened to the equally distinct but relatively small marks of the panther's coat. As a consequence, a change also takes place among the inferior people. In conformity with the new order, they likewise "molt." This molting, it is true, does not go very deep, but that is not to be expected. We must be satisfied with the attainable. If we should go too far and try to achieve too much, it would lead to unrest and misfortune. For the object of a great revolution is the attainment of clarified, secure conditions ensuring a general stabilization on the basis of what is possible at the moment.
-`,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine at the beginning means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Wrapped in the hide of a yellow cow.
+          <br />
+          <br />
+        </p>
+        Changes ought to be undertaken only when there is nothing else to be
+        done. Therefore at first the utmost restraint is necessary. One must
+        become firm in one’s mind, control oneself—yellow is the color of the
+        mean, and the cow is the symbol of docility—and refrain from doing
+        anything for the time being, because any premature offensive will bring
+        evil results.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the second place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;When one’s own day comes, one may create
+          revolution.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Starting brings good fortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;No blame.
+          <br />
+          <br />
+        </p>
+        When we have tried in every way to bring about reforms, but without
+        success, revolution becomes necessary. But such a thoroughgoing upheaval
+        must be carefully prepared. There must be available a man who has the
+        requisite abilities and who possesses public confidence. To such a man
+        we may well turn. This brings good fortune and is not a mistake. The
+        first thing to be considered is our inner attitude toward the new
+        condition that will inevitably come. We have to go out to meet it, as it
+        were. Only in this way can it be prepared for.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the third place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Starting brings misfortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Perseverance brings danger.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;When talk of revolution has gone the rounds
+          three times,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;One may commit himself,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And men will believe him.
+          <br />
+          <br />
+        </p>
+        When change is necessary, there are two mistakes to be avoided. One lies
+        in excessive haste and ruthlessness, which bring disaster. The other
+        lies in excessive hesitation and conservatism, which are also dangerous.
+        Not every demand for change in the existing order should be heeded. On
+        the other hand, repeated and well-founded complaints should not fail of
+        a hearing. When talk of change has come to one’s ears three times, and
+        has been pondered well, he may believe and acquiesce in it. Then he will
+        meet with belief and will accomplish something.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fourth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Remorse disappears. Men believe him.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Changing the form of government brings good
+          fortune.
+          <br />
+          <br />
+        </p>
+        Radical changes require adequate authority. A man must have inner
+        strength as well as influential position. What he does must correspond
+        with a higher truth and must not spring from arbitrary or petty motives;
+        then it brings great good fortune. If a revolution is not founded on
+        such inner truth, the results are bad, and it has no success. For in the
+        end men will support only those undertakings which they feel
+        instinctively to be just.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fifth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The great man changes like a tiger.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Even before he questions the oracle
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He is believed.
+          <br />
+          <br />
+        </p>
+        A tigerskin, with its highly visible black stripes on a yellow ground,
+        shows its distinct pattern from afar. It is the same with a revolution
+        brought about by a great man: large, clear guiding lines become visible,
+        understandable to everyone. Therefore he need not first consult the
+        oracle, for he wins the spontaneous support of the people.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six at the top means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The superior man changes like a panther.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The inferior man molts in the face.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Starting brings misfortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;To remain persevering brings good fortune.
+          <br />
+          <br />
+        </p>
+        After the large and fundamental problems are settled, certain minor
+        reforms, and elaborations of these, are necessary. These detailed
+        reforms may be likened to the equally distinct but relatively small
+        marks of the panther’s coat. As a consequence, a change also takes place
+        among the inferior people. In conformity with the new order, they
+        likewise “molt.” This molting, it is true, does not go very deep, but
+        that is not to be expected. We must be satisfied with the attainable. If
+        we should go too far and try to achieve too much, it would lead to
+        unrest and misfortune. For the object of a great revolution is the
+        attainment of clarified, secure conditions ensuring a general
+        stabilization on the basis of what is possible at the moment.
+        <br />
+      </>,
     ],
   },
   50: {
