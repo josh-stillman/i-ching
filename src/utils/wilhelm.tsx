0f7cc5620a24cc9b99aba06fe3a09d28845bf661
Josh Stillman <joshstillman@gmail.com>
2025-01-22 16:19:54 -0500

    add text for 43

--- src/utils/wilhelm.tsx
+++ src/utils/wilhelm.tsx
@@ -8315,94 +8315,246 @@ The difficulties at the beginning are too great for some persons. They get stuck
     ],
   },
   43: {
-    title: '43. Kuai / Break-through (Resoluteness)',
+    title: '43. Kuai / Break-through (Resoluteness) ䷪',
 
-    text: `above TUI THE JOYOUS, LAKE
-below CH'IEN THE CREATIVE, HEAVEN
-
-This hexagram signifies on the one hand a break-through after a long accumulation of tension, as a swollen river breaks through its dikes, or in the manner of a cloudburst. On the other hand, applied to human conditions, it refers to the time when inferior people gradually begin to disappear. Their influence is on the wane; as a result of resolute action, a change in conditions occurs, a break-through. The hexagram is linked with the third month [April-May].
-
-THE JUDGMENT
-
-BREAK-THROUGH. One must resolutely make the matter known
-At the court of the king.
-It must be announced truthfully. Danger.
-It is necessary to notify one's own city.
-It does not further to resort to arms.
-It furthers one to undertake something.
-
-Even if only one inferior man is occupying a ruling position in a city, he is able to oppress superior men. Even a single passion still lurking in the heart has power to obscure reason. Passion and reason cannot exist side by side-therefore fight without quarter is necessary if the good is to prevail. In a resolute struggle of the good against evil, there are, however, definite rules that must not be disregarded, if it is to succeed. First, resolution must be based on a union of strength and friendliness. Second, a compromise with evil is not possible; evil must under all circumstances be openly discredited. Nor must our own passions and shortcomings be glossed over. Third, the struggle must not be carried on directly by force. If evil is branded, it thinks of weapons, and if we do it the favor of fighting against it blow for blow, we lose in the end because thus we ourselves get entangled in hatred and passion. Therefore it is important to begin at home, to be on guard in our own persons against the faults we have branded. In this way, finding no opponent, the sharp edges of the weapons of evil becomes dulled. For the same reasons we should not combat our own faults directly. As long as we wrestle with them, they continue victorious. Finally, the best way to fight evil is to make energetic progress in the good.
-
-THE IMAGE
-
-The lake has risen up to heaven:
-
-The image of BREAK-THROUGH.
-
-Thus the superior man
-Dispenses riches downward
-And refrains from resting on his virtue.
-
-When the water of a lake has risen up to heaven, there is reason to fear a cloudburst. Taking this as a warning, the superior man forestalls a violent collapse. If a man were to pile up riches for himself alone, without considering others, he would certainly experience a collapse. If a man were to pile up riches for himself alone, without considering others, he would certainly experience a collapse. For all gathering is followed by dispersion. Therefore the superior man begins to distribute while he is accumulating. In the same way, in developing his character he takes care not to become hardened in obstinacy but to remain receptive to impressions by help of strict and continuous self-examination.
-`,
+    text: (
+      <>
+        This hexagram signifies on the one hand a break-through after a long
+        accumulation of tension, as a swollen river breaks through its dikes, or
+        in the manner of a cloudburst. On the other hand, applied to human
+        conditions, it refers to the time when inferior people gradually begin
+        to disappear. Their influence is on the wane; as a result of resolute
+        action, a change in conditions occurs, a break-through. The hexagram is
+        linked with the third month [April–May].
+        <br />
+        <br />
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE JUDGMENT
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;BREAK-THROUGH. One must resolutely make the
+          matter known
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;At the court of the king.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It must be announced truthfully. Danger.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It is necessary to notify one’s own city.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It does not further to resort to arms.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to undertake something.
+          <br />
+          <br />
+        </p>
+        Even if only one inferior man is occupying a ruling position in a city,
+        he is able to oppress superior men. Even a single passion still lurking
+        in the heart has power to obscure reason. Passion and reason cannot
+        exist side by side—therefore fight without quarter is necessary if the
+        good is to prevail.
+        <br />
+        <br />
+        In a resolute struggle of the good against evil, there are, however,
+        definite rules that must not be disregarded, if it is to succeed. First,
+        resolution must be based on a union of strength and friendliness.
+        Second, a compromise with evil is not possible; evil must under all
+        circumstances be openly discredited. Nor must our own passions and
+        shortcomings be glossed over. Third, the struggle must not be carried on
+        directly by force. If evil is branded, it thinks of weapons, and if we
+        do it the favor of fighting against it blow for blow, we lose in the end
+        because thus we ourselves get entangled in hatred and passion. Therefore
+        it is important to begin at home, to be on guard in our own persons
+        against the faults we have branded. In this way, finding no opponent,
+        the sharp edges of the weapons of evil become dulled. For the same
+        reasons we should not combat our own faults directly. As long as we
+        wrestle with them, they continue victorious. Finally, the best way to
+        fight evil is to make energetic progress in the good.
+        <br />
+        <p>
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;THE IMAGE
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The lake has risen up to heaven:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The image of BREAK-THROUGH.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Thus the superior man
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Dispenses riches downward
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And refrains from resting on his virtue.
+          <br />
+          <br />
+        </p>
+        When the water of a lake has risen up to heaven, there is reason to fear
+        a cloudburst. Taking this as a warning, the superior man forestalls a
+        violent collapse. If a man were to pile up riches for himself alone,
+        without considering others, he would certainly experience a collapse.
+        For all gathering is followed by dispersion. Therefore the superior man
+        begins to distribute while he is accumulating. In the same way, in
+        developing his character he takes care not to become hardened in
+        obstinacy but to remain receptive to impressions by help of strict and
+        continuous self-examination.
+        <br />
+        <br />
+      </>
+    ),
     lines: [
-      `Nine at the beginning means:
-
-Mighty in the forward-striding toes.
-When one goes and is not equal to the task,
-One makes a mistake.
-
-In times of resolute advance, the beginning is especially difficult. We feel inspired to press forward but resistance is still strong; therefore we ought to gauge our own strength and venture only so far as we can go with certainty of success. To plunge blindly ahead is wrong, because it is precisely at the beginning that an unexpected setback can have the most disastrous results.
-`,
-      `Nine in the second place means:
-
-A cry of alarm. Arms at evening and at night.
-Fear nothing.
-
-Readiness is everything. Resolution is indissolubly bound up with caution. If an individual is careful and keeps his wits about him, he need not become excited or alarmed. If he is watchful at all times, even before danger is present, he is armed when danger approaches and need not be afraid. The superior man is on his guard against what is not yet in sight and on the alert for what is not yet within hearing; therefore he dwells in the midst of difficulties as thought hey did not exist. If a man develops his character, people submit to him of their own accord. If reason triumphs, the passions withdraw of themselves. To be circumspect and not to forget one's armor is the right way to security.
-`,
-      `Nine in the third place means:
-
-To be powerful in the cheekbones
-Brings misfortune.
-
-The superior man is firmly resolved.
-He walks alone and is caught in the rain.
-He is bespattered,
-And people murmur against him.
-
-No blame.
-
-Here we have a man in an ambiguous situation. While all others are engaged in a resolute fight against all that is inferior, he alone has a certain relationship with an inferior man. If he were to show strength outwardly and turn against this man before the time is ripe, he would only endanger the entire situation, because the inferior man would too quickly have recourse to countermeasures. The task of the superior man becomes extremely difficult here. He must be firmly resolved within himself and, while maintaining association with the inferior man, avoid any participation in his evilness. He will of course be misjudged. It will be thought that he belong to the party of the inferior man. He will be lonely because no one will understand him. His relations with the inferior man will sully him in the eyes of the multitude, and they will turn against him, grumbling. But he can endure this lack of appreciation and makes no mistake, because he remains true to himself.
-`,
-      `Nine in the fourth place means:
-
-There is no skin on his thighs,
-And walking comes hard.
-If a man were to let himself be led like a sheep,
-Remorse would disappear.
-But if these words are heard
-
-They will not be believed.
-
-Here a man is suffering from inner restlessness and cannot abide in his place. He would like to push forward under any circumstances, but encounters insuperable obstacles. Thus his situation entails an inner conflict. This is due to the obstinacy with which he seeks to enforce his will. If he would desist from this obstinacy, everything would go well. But this advice, like so much other good counsel, will be ignored. For obstinacy makes a man unable to hear, for all that he has ears.
-`,
-      `Nine in the fifth place means:
-
-In dealing with weeds,
-Firm resolution is necessary.
-Walking in the middle
-Remains free of blame.
-
-Weeds always grow back again and are difficult to exterminate. So too the struggle against an inferior man in a high position demands firm resolution. One has certain relations with him, hence there is danger that one may give up the struggle as hopeless. But this must not be. One must go on resolutely and not allow himself to be deflected from him course. Only in this way does one remain free of blame.
-`,
-      `Six at the top means:
-
-No cry.
-In the end misfortune comes.
-
-Victory seems to have been achieved. There remains merely a remnant of the evil resolutely to be eradicated as the time demands. Everything looks easy. Just there, however, lies the danger. If we are not on guard, evil will succeed in escaping by means of concealment, and when it has eluded us new misfortunes will develop from the remaining seeds, for evil does not die easily. So too in dealing with the evil in own's own character, one must go to work with thoroughness. If out of carelessness anything were to be overlooked, new evil would arise from it.
-`,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine at the beginning means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Mighty in the forward-striding toes.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;When one goes and is not equal to the task,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;One makes a mistake.
+          <br />
+          <br />
+        </p>
+        In times of a resolute advance, the beginning is especially difficult.
+        We feel inspired to press forward but resistance is still strong;
+        therefore we ought to gauge our own strength and venture only so far as
+        we can go with certainty of success. To plunge blindly ahead is wrong,
+        because it is precisely at the beginning that an unexpected setback can
+        have the most disastrous results.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the second place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;A cry of alarm. Arms at evening and at night.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Fear nothing.
+          <br />
+          <br />
+        </p>
+        Readiness is everything. Resolution is indissolubly bound up with
+        caution. If an individual is careful and keeps his wits about him, he
+        need not become excited or alarmed. If he is watchful at all times, even
+        before danger is present, he is armed when danger approaches and need
+        not be afraid. The superior man is on his guard against what is not yet
+        in sight and on the alert for what is not yet within hearing; therefore
+        he dwells in the midst of difficulties as though they did not exist. If
+        a man develops his character, people submit to him of their own accord.
+        If reason triumphs, the passions withdraw of themselves. To be
+        circumspect and not to forget one’s armor is the right way to security.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the third place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;To be powerful in the cheekbones
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Brings misfortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The superior man is firmly resolved.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He walks alone and is caught in the rain.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He is bespattered,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And people murmur against him.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;No blame.
+          <br />
+          <br />
+        </p>
+        Here we have a man in an ambiguous situation. While all others are
+        engaged in a resolute fight against all that is inferior, he alone has a
+        certain relationship with an inferior man. If he were to show strength
+        outwardly and turn against this man before the time is ripe, he would
+        only endanger the entire situation, because the inferior man would too
+        quickly have recourse to countermeasures. The task of the superior man
+        becomes extremely difficult here. He must be firmly resolved within
+        himself and, while maintaining association with the inferior man, avoid
+        any participation in his vileness. He will of course be misjudged. It
+        will be thought that he belongs to the party of the inferior man. He
+        will be lonely because no one will understand him. His relations with
+        the inferior man will sully him in the eyes of the multitude, and they
+        will turn against him, grumbling. But he can endure this lack of
+        appreciation and makes no mistake, because he remains true to himself.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fourth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;There is no skin on his thighs,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And walking comes hard.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If a man were to let himself be led like a
+          sheep,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Remorse would disappear.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;But if these words are heard
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;They will not be believed.
+          <br />
+          <br />
+        </p>
+        Here a man is suffering from inner restlessness and cannot abide in his
+        place. He would like to push forward under any circumstances, but
+        encounters insuperable obstacles. Thus his situation entails an inner
+        conflict. This is due to the obstinacy with which he seeks to enforce
+        his will. If he would desist from this obstinacy, everything would go
+        well. But this advice, like so much other good counsel, will be ignored.
+        For obstinacy makes a man unable to hear, for all that he has ears.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fifth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;In dealing with weeds,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Firm resolution is necessary.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Walking in the middle
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Remains free of blame.
+          <br />
+          <br />
+        </p>
+        Weeds always grow back again and are difficult to exterminate. So too
+        the struggle against an inferior man in a high position demands firm
+        resolution. One has certain relations with him, hence there is danger
+        that one may give up the struggle as hopeless. But this must not be. One
+        must go on resolutely and not allow himself to be deflected from his
+        course. Only in this way does one remain free of blame.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six at the top means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;No cry.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;In the end misfortune comes.
+          <br />
+          <br />
+        </p>
+        Victory seems to have been achieved. There remains merely a remnant of
+        the evil resolutely to be eradicated as the time demands. Everything
+        looks easy. Just there, however, lies the danger. If we are not on
+        guard, evil will succeed in escaping by means of concealment, and when
+        it has eluded us new misfortunes will develop from the remaining seeds,
+        for evil does not die easily. So too in dealing with the evil in one’s
+        own character, one must go to work with thoroughness. If out of
+        carelessness anything were to be overlooked, new evil would arise from
+        it.
+        <br />
+      </>,
     ],
   },
   44: {
