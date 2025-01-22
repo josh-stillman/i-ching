d95b15822b08c3c9de51bdb1642cc21668a00487
Josh Stillman <joshstillman@gmail.com>
2025-01-22 16:21:48 -0500

    add text for 45

--- src/utils/wilhelm.tsx
+++ src/utils/wilhelm.tsx
@@ -8766,98 +8766,223 @@ The difficulties at the beginning are too great for some persons. They get stuck
     ],
   },
   45: {
-    title: "45. Ts'ui / Gathering Together [Massing]",
+    title: "45. Ts'ui / Gathering Together [Massing] ䷬",
 
-    text: `above TUI THE JOYOUS, LAKE
-below K'UN THE RECEPTIVE, EARTH
-
-This hexagram is related in form and meaning to Pi, HOLDING TOGETHER 8. In the latter, water is over the earth; here a lake is over the earth. But since the lake is a place where water collects, the idea of gathering together is even more strongly expressed here than in the other hexagram. The same idea also arises from the fact that in the present case it is two strong lines (the fourth and the fifth) that bring about the gather together, whereas in the former case one strong line (the fifth) stands in the midst of weak lines.
-
-THE JUDGMENT
-
-GATHERING TOGETHER. Success.
-
-The king approaches his temple.
-It furthers one to see the great man.
-
-This brings success. Perseverance furthers.
-
-To bring great offerings creates good fortune.
-It furthers one to undertake something.
-
-The gathering together of people in large communities is either a natural occurrence, as in the case of the family, or an artificial one, as in the case of the state. The family gathers about the father as its head. The perpetuation of this gathering in groups is achieved through the sacrifice to the ancestors, at which the whole clan is gathered together. Through the collective piety of the living members of the family, the ancestors become so integrated in the spiritual life of the family that it cannot be dispersed or dissolved. Where men are to be gathered together, religious forces are needed. But there must also be a human leader to serve as the center of the group. In order to be able to bring others together, this leader must first of all be collected within himself. Only collective moral force can unite the world. Such great times of unification will leave great achievements behind them. This is the significance of the great offerings that are made. In the secular sphere likewise there is no need of great deeds in the time of GATHERING TOGETHER.
-
-THE IMAGE
-
-Over the earth, the lake:
-
-The image of GATHERING TOGETHER.
-
-Thus the superior man renews his weapons
-In order to meet the unforeseen.
-
-If the water in the lake gathers until it rises above the earth, there is danger of a break-through. Precautions must be taken to prevent this. Similarly where men gather together in great numbers, strife is likely to arise; where possessions are collected, robbery is likely to occur. Thus in the time of GATHERING TOGETHER we must arm promptly to ward off the unexpected. Human woes usually come as a result of unexpected events against which we are not forearmed. If we are prepared, they can be prevented.
-`,
+    text: (
+      <>
+        This hexagram is related in form and meaning to Pi, HOLDING TOGETHER
+        (8). In the latter, water is over the earth; here a lake is over the
+        earth. But since the lake is a place where water collects, the idea of
+        gathering together is even more strongly expressed here than in the
+        other hexagram. The same idea also arises from the fact that in the
+        present case it is two strong lines (the fourth and the fifth) that
+        bring about the gathering together, whereas in the former case one
+        strong line (the fifth) stands in the midst of weak lines.
+        <br />
+        <br />
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE JUDGMENT
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;GATHERING TOGETHER. Success.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The king approaches his temple.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to see the great man.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;This brings success. Perseverance furthers.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;To bring great offerings creates good fortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to undertake something.
+          <br />
+          <br />
+        </p>
+        The gathering together of people in large communities is either a
+        natural occurrence, as in the case of the family, or an artificial one,
+        as in the case of the state. The family gathers about the father as its
+        head. The perpetuation of this gathering in groups is achieved through
+        the sacrifice to the ancestors, at which the whole clan is gathered
+        together. Through the collective piety of the living members of the
+        family, the ancestors become so integrated in the spiritual life of the
+        family that it cannot be dispersed or dissolved.
+        <br />
+        <br />
+        Where men are to be gathered together, religious forces are needed. But
+        there must also be a human leader to serve as the center of the group.
+        In order to be able to bring others together, this leader must first of
+        all be collected within himself. Only collective moral force can unite
+        the world. Such great times of unification will leave great achievements
+        behind them. This is the significance of the great offerings that are
+        made. In the secular sphere likewise there is need of great deeds in the
+        time of GATHERING TOGETHER.
+        <br />
+        <p>
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;THE IMAGE
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Over the earth, the lake:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The image of GATHERING TOGETHER.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Thus the superior man renews his weapons
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;In order to meet the unforeseen.
+          <br />
+          <br />
+        </p>
+        If the water in the lake gathers until it rises above the earth, there
+        is danger of a break-through. Precautions must be taken to prevent this.
+        Similarly where men gather together in great numbers, strife is likely
+        to arise; where possessions are collected, robbery is likely to occur.
+        Thus in the time of GATHERING TOGETHER we must arm promptly to ward off
+        the unexpected. Human woes usually come as a result of unexpected events
+        against which we are not forearmed. If we are prepared, they can be
+        prevented.
+        <br />
+        <br />
+      </>
+    ),
     lines: [
-      `Six at the beginning means:
-
-If you are sincere, but not to the end,
-
-There will sometimes be confusion, sometimes gathering together.
-If you call out,
-
-Then after one grasp of the hand you can laugh again.
-Regret not. Going is without blame.
-
-The situation is this: People desire to gather around a leader to whom they look up. But they are in a large group, by which they allow themselves to be influenced, so that they waver in their decision. Thus they lack a firm center around which to gather. But if expression is given to this need, and if they call for help, one grasp of the hand from the leader is enough to turn away all distress. Therefore they must not allow themselves to be led astray. It is undoubtedly right that they should attach themselves to this leader.
-`,
-      `Six in the second place means:
-
-Letting oneself be drawn
-Brings good fortune and remains blameless.
-If one is sincere,
-It furthers one to bring even a small offering.
-
-In the time of GATHERING TOGETHER, we should make no arbitrary choice of the way. There are secret forces at work, leading together those who belong together. We must yield to this attraction; then we make no mistakes. Where inner relationships exist, no great preparations and formalities are necessary. People understand one another forthwith, just as the Divinity graciously accepts a small offering if it comes from the heart.
-`,
-      `Six in the third place means:
-
-Gathering together amid sighs.
-
-Nothing that would further.
-Going is without blame.
-
-Slight humiliation.
-
-Often a man feels an urge to unite with others, but the individuals around him have already formed themselves into a group, so that he remains isolated. The whole situation proves untenable. Then he ought to choose the way of progress, resolutely allying himself with a man who stands nearer to the center of the group, and can help him to gain admission to the closed circle. This is not a mistake, even though at first his position as an outsider is somewhat humiliating.
-`,
-      `Nine in the fourth place means:
-
-Great good fortune. No blame.
-
-This describes a man who gathers people around him in the name of his ruler. Since he is not striving for any special advantages for himself but is working unselfishly to bring about general unity, his work is crowned with success, and everything becomes as it should be.
-`,
-      `Nine in the fifth place means:
-
-If in gathering together one has position,
-
-This brings no blame.
-If there are some who are not yet sincerely in the work,
-
-Sublime and enduring perseverance is needed.
-
-Then remorse disappears.
-
-When people spontaneously gather around a man, it is only a good. It gives him a certain influence that can be altogether useful.. But of course there is also the possibility that many may gather around him not because of a feeling of confidence but merely because of his influential position. This is certainly to be regretted. The only means of dealing with such people is to gain their confidence through steadfastness an intensified, unswerving devotion to duty. In this way secret mistrust will gradually be overcome, and there will be no occasion for regret.
-`,
-      `Six at the top means:
-
-Lamenting and sighing, floods of tears.
-
-No blame.
-
-It may happen that an individual would like to ally himself with another, but his good intentions are misunderstood. Then he becomes sad and laments. But this is the right course. For it may cause the other person to come to his senses, so that the alliance that has been sought and so painfully missed is after all achieved.
-`,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six at the beginning means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If you are sincere, but not to the end,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;There will sometimes be confusion, sometimes
+          gathering together.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If you call out,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Then after one grasp of the hand you can laugh
+          again.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Regret not. Going is without blame.
+          <br />
+          <br />
+        </p>
+        The situation is this: people desire to gather around a leader to whom
+        they look up. But they are in a large group, by which they allow
+        themselves to be influenced, so that they waver in their decision. Thus
+        they lack a firm center around which to gather. But if expression is
+        given to this need, and if they call for help, one grasp of the hand
+        from the leader is enough to turn away all distress. Therefore they must
+        not allow themselves to be led astray. It is undoubtedly right that they
+        should attach themselves to this leader.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the second place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Letting oneself be drawn
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Brings good fortune and remains blameless.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If one is sincere,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to bring even a small
+          offering.
+          <br />
+          <br />
+        </p>
+        In the time of GATHERING TOGETHER, we should make no arbitrary choice of
+        the way. There are secret forces at work, leading together those who
+        belong together. We must yield to this attraction; then we make no
+        mistakes. Where inner relationships exist, no great preparations and
+        formalities are necessary. People understand one another forthwith, just
+        as the Divinity graciously accepts a small offering if it comes from the
+        heart.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the third place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Gathering together amid sighs.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Nothing that would further.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Going is without blame.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Slight humiliation.
+          <br />
+          <br />
+        </p>
+        Often a man feels an urge to unite with others, but the individuals
+        around him have already formed themselves into a group, so that he
+        remains isolated. The whole situation proves untenable. Then he ought to
+        choose the way of progress, resolutely allying himself with a man who
+        stands nearer to the center of the group, and can help him to gain
+        admission to the closed circle. This is not a mistake, even though at
+        first his position as an outsider is somewhat humiliating.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fourth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Great good fortune. No blame.
+          <br />
+          <br />
+        </p>
+        This describes a man who gathers people around him in the name of his
+        ruler. Since he is not striving for any special advantages for himself
+        but is working unselfishly to bring about general unity, his work is
+        crowned with success, and everything becomes as it should be.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fifth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If in gathering together one has position,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;This brings no blame.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If there are some who are not yet sincerely in
+          the work,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Sublime and enduring perseverance is needed.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Then remorse disappears.
+          <br />
+          <br />
+        </p>
+        When people spontaneously gather around a man, it is only a good. It
+        gives him a certain influence that can be altogether useful. But of
+        course there is also the possibility that many may gather around him not
+        because of a feeling of confidence but merely because of his influential
+        position. This is certainly to be regretted. The only means of dealing
+        with such people is to gain their confidence through steadfastness and
+        intensified, unswerving devotion to duty. In this way secret mistrust
+        will gradually be overcome, and there will be no occasion for regret.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six at the top means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Lamenting and sighing, floods of tears.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;No blame.
+          <br />
+          <br />
+        </p>
+        It may happen that an individual would like to ally himself with
+        another, but his good intentions are misunderstood. Then he becomes sad
+        and laments. But this is the right course. For it may cause the other
+        person to come to his senses, so that the alliance that has been sought
+        and so painfully missed is after all achieved.
+        <br />
+      </>,
     ],
   },
   46: {
