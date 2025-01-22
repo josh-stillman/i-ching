891154babf4309085f6ab387cd1d0fdca28d497c
Josh Stillman <joshstillman@gmail.com>
2025-01-22 16:18:38 -0500

    add text for 42

--- src/utils/wilhelm.tsx
+++ src/utils/wilhelm.tsx
@@ -8101,94 +8101,217 @@ The difficulties at the beginning are too great for some persons. They get stuck
     ],
   },
   42: {
-    title: '42. I / Increase',
+    title: '42. I / Increase ䷩',
 
-    text: `above SUN THE GENTLE, WIND
-below CHêN THE AROUSING, THUNDER
-
-The idea of increase is expressed in the fact that the strong lowest line of the upper trigram has sunk down and taken its place under the lower trigram. This conception also expresses the fundamental idea on which the Book of Changes is based. To rule truly is to serve. A sacrifice of the higher element that produces an increase of the lower is called an out-and-out increase: it indicates the spirit that alone has power to help the world.
-
-THE JUDGMENT
-
-INCREASE. It furthers one
-
-To undertake something.
-It furthers one to cross the great water.
-
-Sacrifice on the part of those above for the increase of those below fills the people with a sense of joy and gratitude that is extremely valuable for the flowering of the commonwealth. When people are thus devoted to their leaders, undertakings are possible, and even difficult and dangerous enterprises will succeed. Therefore in such times of progress and successful development it is necessary to work and make the best use of time. This time resembles that of the marriage of heaven and earth, when the earth partakes of the creative power of heaven, forming and bringing forth living beings. The time of INCREASE does not endure, therefore it must be utilized while it lasts.
-
-THE IMAGE
-
-Wind and thunder: the image of INCREASE.
-
-Thus the superior man:
-If he sees good, he imitates it;
-If he has faults, he rids himself of them.
-
-While observing how thunder and wind increase and strengthen each other, a man can not the way to self-increase and self-improvement. When he discovers good in others, he should imitate it and thus make everything on earth his own. If he perceives something bad in himself, let him rid himself of it. In this way he becomes free of evil. This ethical change represents the most important increase of personality.
-`,
+    text: (
+      <>
+        The idea of increase is expressed in the fact that the strong lowest
+        line of the upper trigram has sunk down and taken its place under the
+        lower trigram. This conception also expresses the fundamental idea on
+        which the Book of Changes is based. To rule truly is to serve.
+        <br />
+        <br />
+        A sacrifice of the higher element that produces an increase of the lower
+        is called an out-and-out increase: it indicates the spirit that alone
+        has power to help the world.
+        <br />
+        <br />
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;THE JUDGMENT
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;INCREASE. It furthers one
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;To undertake something.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to cross the great water.
+          <br />
+          <br />
+        </p>
+        Sacrifice on the part of those above for the increase of those below
+        fills the people with a sense of joy and gratitude that is extremely
+        valuable for the flowering of the commonwealth. When people are thus
+        devoted to their leaders, undertakings are possible, and even difficult
+        and dangerous enterprises will succeed. Therefore in such times of
+        progress and successful development it is necessary to work and make the
+        best use of the time. This time resembles that of the marriage of heaven
+        and earth, when the earth partakes of the creative power of heaven,
+        forming and bringing forth living beings. The time of INCREASE does not
+        endure, therefore it must be utilized while it lasts.
+        <br />
+        <p>
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;THE IMAGE
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Wind and thunder: the image of INCREASE.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Thus the superior man:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If he sees good, he imitates it;
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If he has faults, he rids himself of them.
+          <br />
+          <br />
+        </p>
+        While observing how thunder and wind increase and strengthen each other,
+        a man can note the way to self-increase and self-improvement. When he
+        discovers good in others, he should imitate it and thus make everything
+        on earth his own. If he perceives something bad in himself, let him rid
+        himself of it. In this way he becomes free of evil. This ethical change
+        represents the most important increase of personality.
+        <br />
+        <br />
+      </>
+    ),
     lines: [
-      `Nine at the beginning means:
-
-It furthers one to accomplish great deeds.
-
-Supreme good fortune. No blame.
-
-If great help comes to a man from on high, this increased strength must be used to achieve something great for which he might otherwise never have found energy, or readiness to take responsibility. Great good fortune is produced by selflessness, and in bringing about great good fortune, he remains free of reproach.
-`,
-      `Six in the second place means:
-
-Someone does indeed increase him;
-
-Ten pairs of tortoises cannot oppose it.
-Constant perseverance brings good fortune.
-
-The king presents him before God.
-Good fortune.
-
-A man brings about real increase by producing in himself the conditions for it, that it, through receptivity to and love of the good. Thus the thing for which he strives comes of itself, with the inevitability of natural law. Where increase is thus in harmony with the highest laws of the universe, it cannot be prevented by any constellation of accidents. But everything depends on his not letting unexpected good fortune make him heedless; he must make it his own through inner strength an steadfastness. Then he acquires meaning before God and man, and can accomplish something for the good of the world.
-`,
-      `Six in the third place means:
-
-One is enriched through unfortunate events.
-
-No blame, if you are sincere
-And walk in the middle,
-And report with a seal to the prince.
-
-A time of blessing and enrichment has such powerful effects that even events ordinarily unfortunate must turn out to the advantage of those affected by them. These persons become free of error, and by acting in harmony with truth they gain such inner authority that they exert influence as if sanctioned by the letter and seal.
-`,
-      `Six in the fourth place means:
-
-If you walk in the middle
-And report the prince,
-He will follow.
-It furthers one to be used
-In the removal of the capital.
-
-It is important that there should be men who mediate between leaders and followers. These should be disinterested people, especially in times of increase, since the benefit is to spread from the leader to the people. Nothing of this benefit should be held back in a selfish way; it should really reach those for whom it is intended. This sort of intermediary, who also exercises a good influence on the leader, is especially important in times when it is a matter of great undertakings, decisive for the future and requiring the inner assent of all concerned.
-`,
-      `Nine in the fifth place means:
-
-If in truth you have a kind heart, ask not.
-
-Supreme good fortune.
-
-Truly, kindness will be recognized as your virtue.
-
-True kindness does not count upon nor ask about merit and gratitude but acts from inner necessity. And such a truly kind heart finds itself rewarded in being recognized, and thus the beneficent influence will spread unhindered.
-`,
-      `Nine at the top means:
-
-He brings increase to no one.
-Indeed, someone even strikes him.
-He does not keep his heart constantly steady.
-Misfortune.
-
-The meaning here is that through renunciation those in high place should bring increase to those below. By neglecting this duty and helping no one, they in turn lose the furhtering influence of others and soon find themselves alone. In this way they invite attacks. An attitude not permanently in harmony with the demands of the time will necessarily bring misfortune with it. Confucius says about this line:
-
-The superior man sets his person at rest before he moves; he composes his mind before he speaks; he makes his relations firm before he asks for something. By attending to these three matters, the superior man gains complete security. But if a man is brusque in his movements, others will not cooperate. If he is agitated in his word, they awaken no echo in others. If he asks for something without having fist established relations, it will not be given to him. If no one is with him, those who would harm him draw near.
-`,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine at the beginning means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to accomplish great deeds.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Supreme good fortune. No blame.
+          <br />
+          <br />
+        </p>
+        If great help comes to a man from on high, this increased strength must
+        be used to achieve something great for which he might otherwise never
+        have found energy, or readiness to take responsibility. Great good
+        fortune is produced by selflessness, and in bringing about great good
+        fortune, he remains free of reproach.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the second place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Someone does indeed increase him;
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Ten pairs of tortoises cannot oppose it.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Constant perseverance brings good fortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;The king presents him before God.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Good fortune.
+          <br />
+          <br />
+        </p>
+        A man brings about real increase by producing in himself the conditions
+        for it, that is, through receptivity to and love of the good. Thus the
+        thing for which he strives comes of itself, with the inevitability of
+        natural law. Where increase is thus in harmony with the highest laws of
+        the universe, it cannot be prevented by any constellation of accidents.
+        But everything depends on his not letting unexpected good fortune make
+        him heedless; he must make it his own through inner strength and
+        steadfastness. Then he acquires meaning before God and man, and can
+        accomplish something for the good of the world.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the third place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;One is enriched through unfortunate events.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;No blame, if you are sincere
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And walk in the middle,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And report with a seal to the prince.
+          <br />
+          <br />
+        </p>
+        A time of blessing and enrichment has such powerful effects that even
+        events ordinarily unfortunate must turn out to the advantage of those
+        affected by them. These persons become free of error, and by acting in
+        harmony with truth they gain such inner authority that they exert
+        influence as if sanctioned by letter and seal.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Six in the fourth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If you walk in the middle
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;And report to the prince,
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He will follow.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;It furthers one to be used
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;In the removal of the capital.
+          <br />
+          <br />
+        </p>
+        It is important that there should be men who mediate between leaders and
+        followers. These should be disinterested people, especially in times of
+        increase, since the benefit is to spread from the leader to the people.
+        Nothing of this benefit should be held back in a selfish way; it should
+        really reach those for whom it is intended. This sort of intermediary,
+        who also exercises a good influence on the leader, is especially
+        important in times when it is a matter of great undertakings, decisive
+        for the future and requiring the inner assent of all concerned.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine in the fifth place means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;If in truth you have a kind heart, ask not.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Supreme good fortune.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Truly, kindness will be recognized as your
+          virtue.
+          <br />
+          <br />
+        </p>
+        True kindness does not count upon nor ask about merit and gratitude but
+        acts from inner necessity. And such a truly kind heart finds itself
+        rewarded in being recognized, and thus the beneficent influence will
+        spread unhindered.
+        <br />
+        <br />
+      </>,
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;Nine at the top means:
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He brings increase to no one.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Indeed, someone even strikes him.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;He does not keep his heart constantly steady.
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Misfortune.
+          <br />
+          <br />
+        </p>
+        The meaning here is that through renunciation those in high place should
+        bring increase to those below. By neglecting this duty and helping no
+        one, they in turn lose the furthering influence of others and soon find
+        themselves alone. In this way they invite attacks. An attitude not
+        permanently in harmony with the demands of the time will necessarily
+        bring misfortune with it. K'ung Fu-tzu says about this line:
+        <br />
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;“The superior man sets his person at rest before
+        he moves; he composes his mind before he speaks; he makes his relations
+        firm before he asks for something. By attending to these three matters,
+        the superior man gains complete security. But if a man is brusque in his
+        movements, others will not co-operate. If he is agitated in his words,
+        they awaken no echo in others. If he asks for something without having
+        first established relations, it will not be given to him. If no one is
+        with him, those who would harm him draw near.”
+        <br />
+      </>,
     ],
   },
   43: {
