dcaf0b2f7c38f045450d908c7697d64d92225a83
Josh Stillman <joshstillman@gmail.com>
2025-06-11 15:35:34 -0400

    add static svg background with updated rough-animated

--- src/components/IChingPage/IChingPage.tsx
+++ src/components/IChingPage/IChingPage.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-// import { useEffect, useRef } from 'react';
-// import roughAnimated from 'rough-animated';
+import { useEffect, useRef } from 'react';
+import roughAnimated from 'rough-animated';
 
 import styles from './IChingPage.module.css';
 import { Hexagram } from '@utils/utils';
 import { Hex } from '@components/Hex/Hex';
-// import { useViewport } from '../../hooks/useViewport';
+import { useViewport } from '../../hooks/useViewport';
 import { useSearchParams } from 'next/navigation';
 import { HexTextDisplay } from '../TextDisplay/HexTextDisplay/HexTextDisplay';
 
@@ -29,41 +29,41 @@ const IChingPage = () => {
   });
 
   const changingHex = hexagram.getChangingHex();
-  // const svgRef = useRef<SVGSVGElement>(null);
-  // const { height, width } = useViewport();
+  const svgRef = useRef<SVGSVGElement>(null);
+  const { height, width } = useViewport();
 
-  // useEffect(() => {
-  //   const resetShape = () => {
-  //     if (!svgRef.current) {
-  //       return;
-  //     }
+  useEffect(() => {
+    const resetShape = () => {
+      if (!svgRef.current) {
+        return;
+      }
 
-  //     const rc = roughAnimated.svg(svgRef.current);
+      const rc = roughAnimated.svg(svgRef.current);
 
-  //     svgRef.current.replaceChildren(
-  //       rc.rectangle(0, 0, width, height, {
-  //         animate: false,
-  //         fillStyle: 'hachure',
-  //         hachureGap: 1.5,
-  //         fill: '#e1e5eb',
-  //         stroke: 'none',
-  //       })
-  //     );
-  //   };
+      svgRef.current.replaceChildren(
+        rc.rectangle(0, 0, width, height, {
+          animate: false,
+          fillStyle: 'hachure',
+          hachureGap: 1.5,
+          fill: '#e1e5eb',
+          stroke: 'none',
+        })
+      );
+    };
 
-  //   resetShape();
-  // }, []);
+    resetShape();
+  }, []);
 
   return (
     <main className={styles.iChingPageWrapper}>
-      {/* {height && width && (
+      {height && width && (
         <svg
           className="background"
           width={width}
           height={height}
           ref={svgRef}
         ></svg>
-      )} */}
+      )}
       <section className={styles.hexContainer}>
         <Hex hexagram={hexagram} />
 
